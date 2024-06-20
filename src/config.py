9ac52be6e9607f9c55c73b02f4552ad68fc48d0c
Janne Wernecken <janne.wernecken@study.hs-duesseldorf.de>
2024-06-20 20:07:46 +0200

    add def to pack mel specs and metadata to npy files

--- src/config.py
+++ src/config.py
@@ -4,7 +4,12 @@ metadata_filename = "all_data_updated.csv"
 target_feature = "queen status"
 
 duration_of_audio_file = 60
-sampling_rate = 22050
+sampling_rate = 11025
 number_of_samples_in_audio_file = sampling_rate * duration_of_audio_file
+
+n_fft = 2048
+hop_length = 256
+n_mels = 128
+fmax = int(sampling_rate/2)
 
 number_of_target_columns = 1
--- src/features.py
+++ src/features.py
@@ -22,6 +22,68 @@ def get_matching_raw_filename(file):
     matching_raw_filename = filename_no_segment + '.raw'
     return matching_raw_filename
 
+def create_mel_spectrogram_from_audio_data(audio_data: np.ndarray, sampling_rate=config.sampling_rate, hop_length=config.hop_length, n_mels=config.n_mels):
+    """Creates a mel spectrogram from the given audio data."""
+    mel_spectrogram = librosa.feature.melspectrogram(
+        y=audio_data, sr=sampling_rate, hop_length=hop_length, n_mels=n_mels, fmax=int(sampling_rate/2))
+
+    mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
+
+    return mel_spectrogram
+
+
+def preprocess_data_and_pack_to_npy(audio_files_path, processed_data_path, metadata_path):
+    """Preprocesses the audio files and metadata and packs them into a .npy file.
+
+    Parameters:
+    - audio_files_path (str): Path to the directory containing the audio files.
+    - processed_data_path (str): Path to the directory where the .npy file should be saved.
+    """
+
+    print('Packing audio files to .npy file...')
+
+    if not os.path.exists(processed_data_path):
+        os.makedirs(processed_data_path)
+    packed_np_path_mel_specs = os.path.join(processed_data_path, 'bee_hive_mel_specs.npy')
+    packed_np_path_metadata = os.path.join(processed_data_path, 'bee_hive_metadata.npy')
+
+
+    audio_files = librosa.util.find_files(audio_files_path, ext=['wav'])
+    len_audio_files = len(audio_files)
+    print('Number of audio files:', len_audio_files)
+
+    metadata_df = pd.read_csv(metadata_path)
+
+    mel_specs = []
+    metadatas = []
+    for index, file in enumerate(audio_files):
+        print('--- Preparing file number:', index + 1, 'of', len_audio_files, '---')
+
+        # Get corresponding metadata for the audio file
+        metadata_key = get_matching_raw_filename(file)
+        metadata = metadata_df.loc[metadata_df['file name'] == metadata_key]
+        metadata = np.array(metadata)
+        metadata = np.squeeze(metadata)
+        metadata[16] = os.path.basename(file) # replace the raw file name with the "real" "file name
+
+        # Generate mel spec
+        audio_data, _ = librosa.load(
+            file, duration=config.duration_of_audio_file, sr=None)
+        mel_spectrogram = create_mel_spectrogram_from_audio_data(audio_data, hop_length=256)
+
+        mel_specs.append(mel_spectrogram)
+        metadatas.append(metadata)
+
+    # Write the data to two .npy files
+    mel_specs = np.array(mel_specs)
+    metadata = np.array(metadatas)
+    
+    np.save(packed_np_path_mel_specs, mel_specs)
+    np.save(packed_np_path_metadata, metadata)
+
+
+# preprocess_data_and_pack_to_npy("./data/raw", "./data/processed", "./data/all_data_updated.csv")
+
 
 def pack_audio_files_to_hdf5(audio_files_path, hdf5_file_path):
     if not os.path.exists(hdf5_file_path):
