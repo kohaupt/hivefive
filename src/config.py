1d6f51c36db28a4b2a32d464589fb7b02033448c
Konstantin Haupt <konstantin.haupt@yahoo.de>
2024-07-11 18:12:37 +0200

    fix: Add config-variables for segmentation

--- src/config.py
+++ src/config.py
@@ -13,11 +13,16 @@ PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed') # Processed Data Dire
 NORMALIZED_MEL_SPEC_PATH = os.path.join(PROCESSED_DATA_PATH, 'normalized')
 METADATA_FILE = os.path.join(RAW_DATA_PATH, 'state_labels.csv') # Metadata File
 PROCESSED_METADATA_FILE = os.path.join(PROCESSED_DATA_PATH, 'bee_hive_metadata.npy') # Processed Metadata File
+PROCESSED_METADATA_FILE_SEGMENTED = os.path.join(PROCESSED_DATA_PATH, 'bee_hive_metadata_segmented.npy') # Processed Metadata File
 TARGET_FEATURE = "label"
 
 duration_of_audio_file = 60
 sampling_rate = 22050
 number_of_samples_in_audio_file = sampling_rate * duration_of_audio_file
+
+# Segmentation configuration
+segment_duration = 60.0
+overlap_duration = 1.0
 
 n_fft = 2048
 hop_length = 512
