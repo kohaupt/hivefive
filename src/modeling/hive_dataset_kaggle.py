5fcf1ffded356d6b2ad8b295e7b34ea2e64898a8
Konstantin Haupt <konstantin.haupt@yahoo.de>
2024-09-24 21:13:40 +0200

    feat: Add evaluation for kaggle-dataset + dedicated dataloader & train-classes

---
+++ src/modeling/hive_dataset_kaggle.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from torch.utils.data import Dataset
+import torch
+import os
+import numpy as np
+import torch.nn.functional as F
+
+class HiveDatasetKaggle(Dataset):
+    """
+    A custom PyTorch Dataset class for bee hive mel spectrogram images from Kaggle. This class is designed to handle mel spectrogram datasets.
+
+    Parameters:
+    - metadata (pd.DataFrame): DataFrame containing image metadata (e.g., filenames, labels).
+    - processed_data_path (str): Directory path where processed mel specs are stored (requires .npy files).
+    - target_feature (str): Column name in `metadata` representing the label for each mel spec.
+    - transform (callable, optional): A function/transform that takes in an image and returns a transformed version. E.g., data augmentation procedures.
+    - fake_rgb (bool): If True, the mel spec will be converted to a 3-channel image (RGB) by stacking the same image 3 times.
+
+    Usage:
+    dataset = HiveDataset(metadata=df, processed_data_path="/path/to/images", target_feature='queen_status', fake_rgb=True)
+    """
+
+    def __init__(
+        self,
+        metadata_df: pd.DataFrame,
+        processed_data_path: str,
+        target_feature: str,
+        transform=None,
+        fake_rgb=False
+    ):
+        self.metadata = metadata_df.reset_index()
+        self.target = metadata_df[target_feature].values
+        self.processed_data_path = processed_data_path
+        self.transform = transform
+        self.fake_rgb = fake_rgb
+
+    def __len__(self):
+        """Returns the total number of samples in the dataset."""
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        """Retrieves the mel_spec and its label at the specified index `idx`.
+
+        Parameters:
+        - idx (int): Index of the item to retrieve.
+
+        Returns:
+        - tuple: (mel_spec, label) where image is the mel_spec and label is the target(s).
+        """
+        # Load mel_spec
+        mel_spec_path = os.path.join(self.processed_data_path, self.metadata.iloc[idx]["file name"] + ".npy")
+        mel_spec = np.load(mel_spec_path)
+
+        mel_spec = torch.tensor(mel_spec).float()
+
+        # Bring into shape (1, 128, 5168) for CNN
+        # (channels, height, width)
+        mel_spec = mel_spec.unsqueeze(0)
+
+        # TODO: Should we remove this?
+        # Scale the time dimension to 400
+        mel_spec = F.interpolate(mel_spec.unsqueeze(0), size=(128, 400), mode='bilinear', align_corners=False)
+
+        # TODO: Find a smarter way to do this
+        # Remove the extra dimension added by the interpolation step (1, 1, 128, 400) -> (1, 128, 400)
+        mel_spec = mel_spec.squeeze(0)
+
+        # Fake RGB to be able to use pre-trained models
+        if(self.fake_rgb):
+            # Fake 3 channels:
+            mel_spec = torch.stack([mel_spec[0], mel_spec[0], mel_spec[0]], 0)
+
+        if(self.transform):
+            mel_spec = self.transform(mel_spec)
+
+        label = torch.tensor(self.target[idx]).float()
+
+        return mel_spec, label
---
+++ src/modeling/train_kaggle.py
@@ -0,0 +1,163 @@
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchinfo import summary
+from datetime import datetime
+from tqdm import tqdm
+import numpy as np
+from hive_dataset_kaggle import HiveDatasetKaggle
+import os
+import pandas as pd
+
+import sys
+from pathlib import Path
+# Allow imports from the src directory
+sys.path.append(
+    str(Path(os.path.dirname(os.path.abspath(__file__))).parents[0]))
+import config
+
+class TrainKaggle():
+
+    def __init__(self, model=None, loss_fn=None):
+        # Load metadata
+        metadata_column_names = ['device', 'hive number', 'date', 'hive temp', 'hive humidity','hive pressure', 'weather temp', 'weather humidity', 'weather pressure', 'wind speed', 'gust speed', 'weatherID', 'cloud coverage', 'rain', 'lat', 'long', 'file name', 'queen presence', 'queen acceptance', 'frames', 'target', 'time', 'queen status']
+        metadata = np.load(config.PROCESSED_METADATA_FILE_SEGMENTED, allow_pickle=True)
+        metadata_df = pd.DataFrame(metadata, columns=metadata_column_names)
+
+        metadata_df['file name'] = metadata_df['file name'].apply(lambda x: x.replace('.wav', ''))
+        test_dataset = HiveDatasetKaggle(metadata_df=metadata_df, processed_data_path=config.NORMALIZED_MEL_SPEC_PATH, target_feature='queen presence')
+        self.test_dataloader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=True)
+
+        # Initialize model
+        self.model = model
+
+        # Send model to GPU if available
+        if torch.cuda.is_available():
+            self.model.cuda()
+        print(summary(self.model))
+
+        # Initialize loss function and optimizer
+        self.loss_fn = loss_fn
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
+
+
+    def train_one_epoch(self, epoch_index, tb_writer, device="cpu"):
+        running_loss = 0.
+        avg_loss = 0.
+
+        device = torch.device(device)
+
+        for i, data in enumerate(tqdm(self.training_dataloader)):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # Zero your gradients for every batch!
+            self.optimizer.zero_grad()
+
+            # Make predictions for this batch
+            outputs = self.model(inputs)
+            outputs = outputs.squeeze(1)
+
+            # Compute the loss and its gradients
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+
+            # Adjust learning weights
+            self.optimizer.step()
+
+            # Gather data and report
+            running_loss += loss.item()
+            avg_loss += loss.item()
+            if (i + 1) % self.training_dataloader.batch_size == 0:
+                last_loss = running_loss / self.training_dataloader.batch_size  # loss per batch
+                # print(f"  batch {i +1} loss: {last_loss:.3f}")
+                tb_x = epoch_index * len(self.training_dataloader) + i + 1
+                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+                running_loss = 0.
+
+        return avg_loss / (i + 1)
+
+
+    def evaluate(self, device="cpu"):
+        running_vloss = 0.0
+
+        # Set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        self.model.eval()
+
+        # Disable gradient computation and reduce memory consumption.
+        with torch.no_grad():
+            for i, vdata in enumerate(self.validation_dataloader):
+                if i > 10:
+                    break
+                vinputs, vlabels = vdata
+                vinputs, vlabels = vinputs.to(device), vlabels.to(device)
+                voutputs = self.model(vinputs)
+                voutputs = voutputs.squeeze(1)
+                vloss = self.loss_fn(voutputs, vlabels)
+                running_vloss += vloss
+        return running_vloss / (i + 1)
+
+
+    def train(self, epochs):
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        writer = SummaryWriter('runs/cnn_trainer_{}'.format(timestamp))
+        epoch_number = 0
+
+        best_vloss = np.inf
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Training will happen on {device}.")
+
+        if not os.path.exists(config.MODEL_INTERIM_PATH):
+            os.makedirs(config.MODEL_INTERIM_PATH)
+
+        for epoch in range(epochs):
+            print(f"EPOCH {epoch_number + 1}")
+
+            # Make sure gradient tracking is on, and do a pass over the data
+            self.model.train(True)
+            avg_loss_train = self.train_one_epoch(epoch_number, writer, device)
+
+            # Evaluate the model on the validation set
+            avg_loss_val = self.evaluate(device)            
+            print(f"LOSS train: {avg_loss_train:.3f}, val: {avg_loss_val:.3f}")
+
+            # Log the running loss averaged per batch
+            # for both training and validation
+            writer.add_scalars('Training vs. Validation Loss',
+                            { 'Training' : avg_loss_train, 'Validation' : avg_loss_val },
+                            epoch_number + 1)
+            writer.flush()
+
+            # Track best performance, and save the model's state
+            if avg_loss_val < best_vloss:
+                best_vloss = avg_loss_val
+                checkpoint_path = os.path.join(config.MODEL_INTERIM_PATH, f"model_{timestamp}_{epoch_number}_checkpoint.pt")
+                model_path = os.path.join(config.MODEL_INTERIM_PATH, f"model_{timestamp}_{epoch_number}.pt")
+
+                checkpoint = {
+                    'epoch': epoch,
+                    'state_dict': self.model.state_dict(),
+                    'optimizer': self.optimizer.state_dict(),
+                    'train_samples': self.train_samples,
+                    'val_samples': self.val_samples,
+                    'test_samples': self.test_samples,
+                }
+
+                torch.save(checkpoint, checkpoint_path)
+                torch.save(self.model.state_dict(), model_path)
+
+            epoch_number += 1
+        writer.close()
+
+
+    def load_model(self, checkpoint_path, model, optimizer):
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        train_samples = checkpoint['train_samples']
+        val_samples = checkpoint['val_samples']
+        test_samples = checkpoint['test_samples']
+        return model, optimizer, checkpoint['epoch'], train_samples, val_samples, test_samples
